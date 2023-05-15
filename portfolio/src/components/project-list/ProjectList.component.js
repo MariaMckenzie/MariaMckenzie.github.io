04e363bb051280d9122e017b76479fb80f20cb16
Maria McKenzie <mckenzie.maria.a@gmail.com>
2023-05-14 21:49:22 -0500

    Created project list

--- portfolio/src/components/project-list/ProjectList.component.js
+++ portfolio/src/components/project-list/ProjectList.component.js
@@ -1,22 +1,20 @@
 import React from "react"; 
-import { Card, Container, Nav, Navbar } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
-import styles from "./ProjectList.module.css";
+import { Row, Col } from "react-bootstrap";
+import ProjectCardComponent from "../project-card/ProjectCard.component";
+// import styles from "./ProjectList.module.css";
 
 const projectListComponent = () => {
     return (
         <>
-            <LinkContainer  to="/projects/uiux/1">
-                <Card className={ [styles.card, "m-2 text-center"].join(" ") } >
-                    <Card.Img src={ require(`../../assets/images/logo.png`) } alt="image" className= { [styles.img, "d-flex flex-row"].join(" ") } />
-                    <Card.Body>
-                        <Card.Title className={styles.heading} >Name</Card.Title>
-                        <Card.Text>
-                            $100.00
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            </LinkContainer>
+            <Row className="row-cols-auto g-2 m-0 p-0">
+                    {
+                        Array.from({ length: 5 }).map((_, id) => (
+                            <Col>
+                                <ProjectCardComponent key={`project${id}`} />
+                            </Col>
+                        ))
+                    }
+            </Row>
         </>
     )
 }
