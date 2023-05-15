d9632b6a48ebf2fe8550d03880dc02edc745d538
Maria McKenzie <mckenzie.maria.a@gmail.com>
2023-05-15 08:28:08 -0500

    Update index.js

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -2,17 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { RouterProvider, createHashRouter } from 'react-router-dom';
 
-const router = createHashRouter([
-    {
-        path: "/*",
-        element: <App />
-    }
-])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
