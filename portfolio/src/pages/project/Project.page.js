fef9126f63d54f8405f19238dd455960994f6882
Maria McKenzie <mckenzie.maria.a@gmail.com>
2023-05-14 21:53:22 -0500

    create project page

---
+++ portfolio/src/pages/project/Project.page.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import styles from "./Project.module.css";
+
+const portfolioPage = () => {
+    return (
+        <>
+            <Container className="m-0 p-0">
+                <h4>Projects</h4>
+            </Container>
+            <Container>
+                Tab
+            </Container>
+            <Container>
+                Projects
+            </Container>
+        </>
+    )
+}
+
+export default portfolioPage; 
