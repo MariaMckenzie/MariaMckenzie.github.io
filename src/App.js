33e2aa0897963a862b45cf78e6c37aabae51a899
Maria McKenzie <mckenzie.maria.a@gmail.com>
2023-05-16 10:11:45 -0500

    update route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, HashRouter } from "react-router-dom";
 import HomePage from "./pages/home/Home.page";
 import PortfolioPage from "./pages/portfolio/Portfolio.page";
 import ProjectPage from "./pages/project/Project.page";
@@ -10,7 +10,8 @@ import NavComponent from "./components/nav/Nav.component";
 function App() {
     return (
         <>
-            <BrowserRouter basename="https://mariamckenzie.github.io/">
+            <BrowserRouter>
+            <HashRouter basename="/">
             <NavComponent/>
             <Routes>
                 {/* Routes */}
@@ -22,6 +23,7 @@ function App() {
                 {/* Error Page */}
                 <Route path="*" element= { <ErrorPage/> } />
             </Routes>
+            </HashRouter>
             </BrowserRouter>
         </>
     );
