3298f9b07fb3414925e0b63f2d80552e142e5382
Maria McKenzie <mckenzie.maria.a@gmail.com>
2023-05-20 21:40:39 -0500

    add cards using loop and remove unused imports

--- src/components/intro/intro.component.js
+++ src/components/intro/intro.component.js
@@ -1,24 +1,22 @@
 import React from "react";
 import data from "../../assets/data/intro.data.json";
-import { Button, Col, Container, Image, Row } from "react-bootstrap";
+import { Button, Col, Image, Row } from "react-bootstrap";
 import img from "../../assets/images/pic1.png";
-import styles from "./intro.module.css";
+import styles from "./styles.module.css";
 
 function IntroComponent() {
     return (
-        <Container className="p-5">
-            <Row>
-                <Col lg={7}>
-                    <h4 className={ styles.heading }>Hello I'm { data.name.split(" ")[0] }</h4>
-                    <h3 className={ styles.subheading }>{ data.about }</h3>
-                    <Button className={ styles.btn1 }>View Projects</Button>
-                    <Button className={ styles.btn2 }>Contact Me</Button>
-                </Col>
-                <Col className="text-center">
-                    <Image src={img} alt="profile image" fluid className={styles.img} />
-                </Col>
-            </Row>
-        </Container>
+        <Row className="m-0 p-5 align-items-center">
+            <Col lg={7} className="p-0">
+                <h4 className={ styles.heading }>Hello I'm { data.name.split(" ")[0] }</h4>
+                <h3 className={ styles.subheading }>{ data.about }</h3>
+                <Button className={ styles.btn1 }>View Projects</Button>
+                <Button className={ styles.btn2 }>Contact Me</Button>
+            </Col>
+            <Col className="text-center p-0">
+                <Image src={img} alt="profile image" fluid className={styles.img} />
+            </Col>
+        </Row>
     )
 }
 
