fc659ac490c804aefce499eb8227e9237a6481a6
Maria McKenzie <mckenzie.maria.a@gmail.com>
2023-05-29 18:59:32 -0500

    Update Nav.component.js

--- src/components/nav/Nav.component.js
+++ src/components/nav/Nav.component.js
@@ -2,14 +2,14 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { Container, Image, Nav, Navbar } from "react-bootstrap";
 import logo from "../../assets/images/logo.png";
-import styles from "./styles.module.css";
+import styles from "./nav.module.css";
 
-function NavComponent () {
+function NavComponent ( activeLink ) {
     return (
         <Navbar expand="lg" className={ [styles.nav, "container-fluid mx-auto" ].join(" ") }>
             <Container className="mx-3 navbar-nav">
                 <Navbar.Brand className="mx-3" href="/">
-                    <Image src={logo} alt="brand logo" className={styles.logo} />
+                    <Image src={logo} alt="brand logo" className={ styles.logo } />
                 </Navbar.Brand>
 
                 {/* toggle navbar between expanded form and hamburger menu */}
@@ -17,10 +17,12 @@ function NavComponent () {
                 <Navbar.Collapse className={ styles.collapseNav }> {/* add styles for hamburger menu */}
                 
                 <Nav className="ms-auto">
-                    <NavLink to="/" className={ styles.link }>Home</NavLink>
+                    {/* check to see if the link is active. may need to fix but this will solve the issue of the active not being displayed */}
+                    <NavLink to="/" className={styles.link }>Home</NavLink>
                     <NavLink to="/portfolio" className={ styles.link }>Portfolio</NavLink>
                     <NavLink to="/resume" className={ styles.link }>Resume</NavLink>
-                    <NavLink to="/contact-me" className={ styles.link }>Contact</NavLink>
+                    {/* <NavLink to="/certificates" className={ styles.link }>Certificates</NavLink> */}
+                    {/* <NavLink to="/contact-me" className={ styles.link }>Contact</NavLink> */}
                 </Nav>
                 </Navbar.Collapse>
             </Container>
