06b54673933b53a5cbdad7b66cc4fe8f3089617e
Maria McKenzie <mckenzie.maria.a@gmail.com>
2023-05-29 18:59:59 -0500

    Update whatIDo.component.js

--- src/components/what-i-do/whatIDo.component.js
+++ src/components/what-i-do/whatIDo.component.js
@@ -1,21 +1,22 @@
 import React from "react";
 import data from "../../assets/data/intro.data.json";
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
-import styles from "./styles.module.css";
+import styles from "./whatIDo.module.css";
 
 function WhatIDoComponent() {
     return (
-        <Container className="p-5 my-4 text-center">
-            <h4 className={ styles.heading }>What I Do</h4>
-            <Row className="row-cols-auto text-center justify-content-center">
+        <Container className="m-o p-5">
+            <h3 className={ styles.heading }>What I Do</h3>
+            <Row className="row-cols-auto text-start">
                 {/* add cards from json file */}
                     {
                         Array.from({ length: data.role.length }).map((_, id) => (
-                            <Col>
+                            <Col className="me-auto">
                                 <Card className={ styles.card }>
+                                    <Container className={ styles.cardDecor }></Container>
                                     <Card.Title className={ styles.title }> { data.role[id].name } </Card.Title>
                                     <Card.Text className={ styles.text }> { data.role[id].info } </Card.Text>
-                                    <Button className={ styles.btn }>View  Projects</Button>
+                                    <Button className={ styles.btn }>View Projects</Button>
                                 </Card>
                             </Col>
                         ))
