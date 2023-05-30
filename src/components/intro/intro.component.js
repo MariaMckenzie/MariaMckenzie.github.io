66108a6d0d3e16781b77ee60a5064ab7c077e90d
Maria McKenzie <mckenzie.maria.a@gmail.com>
2023-05-30 16:56:13 -0500

    fix styles

--- src/components/intro/intro.component.js
+++ src/components/intro/intro.component.js
@@ -11,15 +11,15 @@ function IntroComponent() {
         <Row className="m-0 p-5 align-items-center">
             <Col lg={7} className="p-0">
                 <h4 className={ styles.heading }>Hello I'm { data.name.split(" ")[0] }</h4>
-                <h3 className={ styles.subheading }>{ data.about }</h3>
+                <h2 className={ styles.subheading }>{ data.about }</h2>
 
                 <Container className="py-3 d-flex px-0">
                     <Image src={githubIcon} alt="Github Icon" fluid className={ styles.icons } />
                     <Image src={linkedinIcon} alt="LinkedIn Icon" fluid className={ styles.icons } />
                 </Container>
 
-                <Button className={ styles.btn1 }>View Projects</Button>
-                <Button className={ styles.btn2 }>Contact Me</Button>
+                <Button className={ styles.btn1 }>View Portfolio</Button>
+                <Button className={ styles.btn2 }>View Resume</Button>
             </Col>
             <Col className="text-center p-0">
                 <Image src={img} alt="profile image" fluid className={styles.img} />
