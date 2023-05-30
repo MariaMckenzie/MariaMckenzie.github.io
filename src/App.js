99478018ad1574b2699c873cabc51b1dafe2d91b
Maria McKenzie <mckenzie.maria.a@gmail.com>
2023-05-29 22:22:05 -0500

    update route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomePage from "./pages/home/Home.page";
 import PortfolioPage from "./pages/portfolio/Portfolio.page";
 import ProjectPage from "./pages/project/Project.page";
 import ContactPage from "./pages/contact/Contact.page";
+import ResumePage from "./pages/resume/Resume.page";
 import ErrorPage from "./pages/error/Error.page";
 
 function App() {
@@ -14,17 +15,11 @@ function App() {
 				<Routes>
 					{/* Routes */}
 					<Route path="/" element={<HomePage />} />
-					<Route
-						path="/portfolio"
-						exact
-						element={<PortfolioPage />}
-					/>
+					<Route path="/home" element={<HomePage />} />
+					<Route path="/portfolio" exact element={<PortfolioPage />} />
+                    <Route path="/resume" exact element={<ResumePage />} />
 					<Route path="/contact-me" element={<ContactPage />} />
-					<Route
-						path="/project/:type/:id"
-						element={<ProjectPage />}
-					/>
-
+					<Route path="/project/:type/:id" element={<ProjectPage />}  />
 					{/* Error Page */}
 					<Route path="/*" element={<ErrorPage />} />
 				</Routes>
