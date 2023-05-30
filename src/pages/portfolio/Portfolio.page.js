8ff6d61caa161c904b422a73c1a4e93ea1868b8b
Maria McKenzie <mckenzie.maria.a@gmail.com>
2023-05-29 22:23:44 -0500

    add fake text as placeholder

--- src/pages/portfolio/Portfolio.page.js
+++ src/pages/portfolio/Portfolio.page.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Image, Row } from "react-bootstrap";
 import data from "../../assets/data/about.data.json";
 import qa from "../../assets/data/qa-projects.data.json";
 import ui from "../../assets/data/ui-projects.data.json";
 import dev from "../../assets/data/dev-projects.data.json";
+import img from "../../assets/images/temp.png";
 import NavComponent from "../../components/nav/nav.component";
 import FooterComponent from "../../components/footer/footer.component";
 import styles from "./Portfolio.module.css";
 
 function PortfolioPage () {
+    const fakeText = "Lorem ipsum dolor sit amet. Vel ipsa quis ea voluptas aliquam est explicabo maiores vel voluptate explicabo est velit odio At qui neque illum. Qui doloremque dolor ad tenetur eveniet sed facere galisum? Sit nemo doloribus eos distinctio dolorem 33 sapiente officiis qui tempora consequuntur aut sunt ratione et optio velit. Aut possimus eius et atione excepturi.";
+    const fakeTitle = "Lorem ipsum dolor sit amet";
+    
     return (
         <Container className="p-4">
             {/* navigation bar */}
@@ -20,22 +24,27 @@ function PortfolioPage () {
 
             {/* projects section */}
             <Row className="py-2">
-                <h4 className={ styles.heading }>Portfolio</h4>
+                <h3 className={ styles.heading }>Portfolio</h3>
 
                 {/* ui ux projects */}
                 <Container>
                     {/* heading */}
-                    <h5>{ data.projects[0] }</h5>
+                    <h6 className={ styles.subheading}>{ data.projects[0] }</h6>
 
                     {/* add project info from json file */
                         Array.from({ length: ui.length }).map((_, id) => (
-                            <Row className="pt-3">
+                            <Row className="pt-3 mx-3">
                                 <Container>
-                                    <Card className={ styles.card }>
-                                        <Card.Title className={ styles.title }> { ui[id].title } </Card.Title>
-                                        {/* <Card.Text className={ styles.text }> { data.role[id].info } </Card.Text> */}
-                                        <Button className={ styles.btn }>View Details</Button>
-                                    </Card>
+                                    <Row className={ styles.card }>
+                                        <Col lg={3} className="align-self-center">
+                                            <Image src={img} alt="LinkedIn Icon" fluid className={ styles.img } />
+                                        </Col>
+                                        <Col className="align-self-center">
+                                            <h6 className={ styles.title }>{ fakeTitle }</h6>
+                                            <p className={ styles.text }>{ fakeText }</p>
+                                            <Button className={ styles.btn }>View Details</Button>
+                                        </Col>
+                                    </Row>
                                 </Container>
                             </Row>
                         ))
@@ -45,17 +54,22 @@ function PortfolioPage () {
                 {/* frontend dev projects */}
                 <Container>
                     {/* heading */}
-                    <h5>{ data.projects[1] }</h5>
+                    <h6 className={ styles.subheading}>{ data.projects[1] }</h6>
 
                     {/* add project info from json file */
-                        Array.from({ length: dev.length }).map((_, id) => (
-                            <Row className="pt-3">
+                        Array.from({ length: ui.length }).map((_, id) => (
+                            <Row className="pt-3 mx-3">
                                 <Container>
-                                    <Card className={ styles.card }>
-                                        <Card.Title className={ styles.title }> { dev[id].title } </Card.Title>
-                                        {/* <Card.Text className={ styles.text }> { data.role[id].info } </Card.Text> */}
-                                        <Button className={ styles.btn }>View Details</Button>
-                                    </Card>
+                                    <Row className={ styles.card }>
+                                        <Col lg={3} className="align-self-center">
+                                            <Image src={img} alt="LinkedIn Icon" fluid className={ styles.img } />
+                                        </Col>
+                                        <Col className="align-self-center">
+                                            <h6 className={ styles.title }>{ fakeTitle }</h6>
+                                            <p className={ styles.text }>{ fakeText }</p>
+                                            <Button className={ styles.btn }>View Details</Button>
+                                        </Col>
+                                    </Row>
                                 </Container>
                             </Row>
                         ))
@@ -65,17 +79,22 @@ function PortfolioPage () {
                 {/* automation projects */}
                 <Container>
                     {/* heading */}
-                    <h5>{ data.projects[2] }</h5>
+                    <h6 className={ styles.subheading}>{ data.projects[2] }</h6>
 
                     {/* add project info from json file */
-                        Array.from({ length: qa.length }).map((_, id) => (
-                            <Row className="pt-3">
+                        Array.from({ length: ui.length }).map((_, id) => (
+                            <Row className="pt-3 mx-3">
                                 <Container>
-                                    <Card className={ styles.card }>
-                                        <Card.Title className={ styles.title }> { qa[id].overview } </Card.Title>
-                                        {/* <Card.Text className={ styles.text }> { data.role[id].info } </Card.Text> */}
-                                        <Button className={ styles.btn }>View Details</Button>
-                                    </Card>
+                                    <Row className={ styles.card }>
+                                        <Col lg={3} className="align-self-center">
+                                            <Image src={img} alt="LinkedIn Icon" fluid className={ styles.img } />
+                                        </Col>
+                                        <Col className="align-self-center">
+                                            <h6 className={ styles.title }>{ fakeTitle }</h6>
+                                            <p className={ styles.text }>{ fakeText }</p>
+                                            <Button className={ styles.btn }>View Details</Button>
+                                        </Col>
+                                    </Row>
                                 </Container>
                             </Row>
                         ))
@@ -85,7 +104,7 @@ function PortfolioPage () {
 
             {/* footer */}
             <Container className={ styles.container }>
-                <Row className="mt-4">
+                <Row className="mt-5">
 					<FooterComponent />
 				</Row>
             </Container>
