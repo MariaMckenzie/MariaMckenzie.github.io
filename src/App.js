c7981fcac376a1ab45cadad3f11aea14a907ae97
Maria McKenzie <mckenzie.maria.a@gmail.com>
2023-05-29 19:01:28 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import "./App.css";
+import { Container } from "react-bootstrap";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import HomePage from "./pages/home/Home.page";
 import PortfolioPage from "./pages/portfolio/Portfolio.page";
 import ProjectPage from "./pages/project/Project.page";
 import ContactPage from "./pages/contact/Contact.page";
 import ErrorPage from "./pages/error/Error.page";
-import NavComponent from "./components/nav/Nav.component";
 
 function App() {
-    return (
-        <>
-            <BrowserRouter basename="/">
-            <NavComponent/>
-            <Routes>
-                {/* Routes */}
-                <Route path="/" element= { <HomePage/> } />
-                <Route path="/portfolio" exact element= { <PortfolioPage/> } />
-                <Route path="/contact-me" element= { <ContactPage/> } />
-                <Route path="/project/:type/:id" element= { <ProjectPage/> } />
+	return (
+		<Container classname="app">
+			<BrowserRouter basename="/">
+				<Routes>
+					{/* Routes */}
+					<Route path="/" element={<HomePage />} />
+					<Route
+						path="/portfolio"
+						exact
+						element={<PortfolioPage />}
+					/>
+					<Route path="/contact-me" element={<ContactPage />} />
+					<Route
+						path="/project/:type/:id"
+						element={<ProjectPage />}
+					/>
 
-                {/* Error Page */}
-                <Route path="*" element= { <ErrorPage/> } />
-            </Routes>
-            </BrowserRouter>
-        </>
-    );
+					{/* Error Page */}
+					<Route path="/*" element={<ErrorPage />} />
+				</Routes>
+			</BrowserRouter>
+		</Container>
+	);
 }
 
 export default App;
