5e32c0ac6070e6882aa276ede90faf4e874ebf26
Maria McKenzie <mckenzie.maria.a@gmail.com>
2023-05-30 16:55:02 -0500

    add components for each project type

--- src/pages/portfolio/Portfolio.page.js
+++ src/pages/portfolio/Portfolio.page.js
@@ -1,18 +1,13 @@
 import React from "react";
-import { Button, Col, Container, Image, Row } from "react-bootstrap";
-import data from "../../assets/data/about.data.json";
-import qa from "../../assets/data/qa-projects.data.json";
-import ui from "../../assets/data/ui-projects.data.json";
-import dev from "../../assets/data/dev-projects.data.json";
-import img from "../../assets/images/temp.png";
+import { Container, Row } from "react-bootstrap";
 import NavComponent from "../../components/nav/nav.component";
+import QAProjectListComponent from "../../components/project-list/qa.component";
+import UIProjectListComponent from "../../components/project-list/ui.component";
+import DevProjectListComponent from "../../components/project-list/dev.component";
 import FooterComponent from "../../components/footer/footer.component";
 import styles from "./Portfolio.module.css";
 
 function PortfolioPage () {
-    const fakeText = "Lorem ipsum dolor sit amet. Vel ipsa quis ea voluptas aliquam est explicabo maiores vel voluptate explicabo est velit odio At qui neque illum. Qui doloremque dolor ad tenetur eveniet sed facere galisum? Sit nemo doloribus eos distinctio dolorem 33 sapiente officiis qui tempora consequuntur aut sunt ratione et optio velit. Aut possimus eius et atione excepturi.";
-    const fakeTitle = "Lorem ipsum dolor sit amet";
-    
     return (
         <Container className="p-4">
             {/* navigation bar */}
@@ -23,84 +18,10 @@ function PortfolioPage () {
             </Container>
 
             {/* projects section */}
-            <Row className="py-2">
-                <h3 className={ styles.heading }>Portfolio</h3>
-
-                {/* ui ux projects */}
-                <Container>
-                    {/* heading */}
-                    <h6 className={ styles.subheading}>{ data.projects[0] }</h6>
-
-                    {/* add project info from json file */
-                        Array.from({ length: ui.length }).map((_, id) => (
-                            <Row className="pt-3 mx-3">
-                                <Container>
-                                    <Row className={ styles.card }>
-                                        <Col lg={3} className="align-self-center">
-                                            <Image src={img} alt="LinkedIn Icon" fluid className={ styles.img } />
-                                        </Col>
-                                        <Col className="align-self-center">
-                                            <h6 className={ styles.title }>{ fakeTitle }</h6>
-                                            <p className={ styles.text }>{ fakeText }</p>
-                                            <Button className={ styles.btn }>View Details</Button>
-                                        </Col>
-                                    </Row>
-                                </Container>
-                            </Row>
-                        ))
-                    }
-                </Container>
-                
-                {/* frontend dev projects */}
-                <Container>
-                    {/* heading */}
-                    <h6 className={ styles.subheading}>{ data.projects[1] }</h6>
-
-                    {/* add project info from json file */
-                        Array.from({ length: ui.length }).map((_, id) => (
-                            <Row className="pt-3 mx-3">
-                                <Container>
-                                    <Row className={ styles.card }>
-                                        <Col lg={3} className="align-self-center">
-                                            <Image src={img} alt="LinkedIn Icon" fluid className={ styles.img } />
-                                        </Col>
-                                        <Col className="align-self-center">
-                                            <h6 className={ styles.title }>{ fakeTitle }</h6>
-                                            <p className={ styles.text }>{ fakeText }</p>
-                                            <Button className={ styles.btn }>View Details</Button>
-                                        </Col>
-                                    </Row>
-                                </Container>
-                            </Row>
-                        ))
-                    }
-                </Container>
-
-                {/* automation projects */}
-                <Container>
-                    {/* heading */}
-                    <h6 className={ styles.subheading}>{ data.projects[2] }</h6>
-
-                    {/* add project info from json file */
-                        Array.from({ length: ui.length }).map((_, id) => (
-                            <Row className="pt-3 mx-3">
-                                <Container>
-                                    <Row className={ styles.card }>
-                                        <Col lg={3} className="align-self-center">
-                                            <Image src={img} alt="LinkedIn Icon" fluid className={ styles.img } />
-                                        </Col>
-                                        <Col className="align-self-center">
-                                            <h6 className={ styles.title }>{ fakeTitle }</h6>
-                                            <p className={ styles.text }>{ fakeText }</p>
-                                            <Button className={ styles.btn }>View Details</Button>
-                                        </Col>
-                                    </Row>
-                                </Container>
-                            </Row>
-                        ))
-                    }
-                </Container>
-            </Row>
+            <h3 className={ styles.heading }>Portfolio</h3>
+            <UIProjectListComponent />
+            <DevProjectListComponent />
+            <QAProjectListComponent />
 
             {/* footer */}
             <Container className={ styles.container }>
