62a0b5a755b820aa1ede05ad17706581cd168c24
Maria McKenzie <mckenzie.maria.a@gmail.com>
2023-05-30 16:55:35 -0500

    fix line spacing/tabs

--- src/components/what-i-do/whatIDo.component.js
+++ src/components/what-i-do/whatIDo.component.js
@@ -8,19 +8,18 @@ function WhatIDoComponent() {
         <Container className="m-o p-5">
             <h3 className={ styles.heading }>What I Do</h3>
             <Row className="row-cols-auto text-start">
-                {/* add cards from json file */}
-                    {
-                        Array.from({ length: data.role.length }).map((_, id) => (
-                            <Col className="me-auto">
-                                <Card className={ styles.card }>
-                                    <Container className={ styles.cardDecor }></Container>
-                                    <Card.Title className={ styles.title }> { data.role[id].name } </Card.Title>
-                                    <Card.Text className={ styles.text }> { data.role[id].info } </Card.Text>
-                                    <Button className={ styles.btn }>View Projects</Button>
-                                </Card>
-                            </Col>
-                        ))
-                    }
+                {/* add cards from json file */
+                    Array.from({ length: data.role.length }).map((_, id) => (
+                        <Col className="me-auto">
+                            <Card className={ styles.card }>
+                                <Container className={ styles.cardDecor }></Container>
+                                <Card.Title className={ styles.title }> { data.role[id].name } </Card.Title>
+                                <Card.Text className={ styles.text }> { data.role[id].info } </Card.Text>
+                                <Button className={ styles.btn }>View Projects</Button>
+                            </Card>
+                        </Col>
+                    ))
+                }
             </Row>
         </Container>
     )
