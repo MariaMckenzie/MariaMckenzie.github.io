b2dc18c42380dc491e78c0914ec32784c0fbceb4
Maria McKenzie <mckenzie.maria.a@gmail.com>
2023-06-01 08:05:33 -0500

    fix routes

--- src/components/intro/intro.component.js
+++ src/components/intro/intro.component.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Button, Col, Container, Image, Row } from "react-bootstrap";
+import { Col, Container, Image, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import intro_data from "../../assets/data/intro.data.json";
 import data from "../../assets/data/about.data.json";
 import img from "../../assets/images/pic1.png";
@@ -14,7 +15,7 @@ function IntroComponent() {
 
             {/* show for smaller screens */}
             <Col className="d-lg-none d-xxl-block pb-4">
-                <Image src={img} alt="profile image" fluid className={styles.img} />
+                <Image src={ img } alt="profile image" fluid className={ styles.img } />
             </Col>
 
             <Col lg={7} className="p-0">
@@ -36,8 +37,8 @@ function IntroComponent() {
 
                 {/* buttons */}
                 <Container className="pt-1 d-block px-0 justify-content-center d-lg-none d-xxl-block pb-4">
-                    <Button href="/portfolio" className={ styles.btn1 }>View Portfolio</Button>
-                    <Button href="/resume" className={ styles.btn2 }>View Resume</Button>
+                    <Link to="/portfolio" className={ styles.btn1 }>View Portfolio</Link>
+                    <Link to="/resume" className={ styles.btn2 }>View Resume</Link>
                 </Container>
 
                 {/* show for larger screens */}
@@ -55,15 +56,15 @@ function IntroComponent() {
 
                 {/* buttons */}
                 <Container className="py-3 d-flex px-0 d-none d-lg-block d-xxl-none">
-                    <Button href="/portfolio" className={ styles.btn1 }>View Portfolio</Button>
-                    <Button href="/resume" className={ styles.btn2 }>View Resume</Button>
+                    <Link to="/portfolio" className={ styles.btn1 }>View Portfolio</Link>
+                    <Link to="/resume" className={ styles.btn2 }>View Resume</Link>
                 </Container>
 
             </Col>
 
             {/* show for larger screens */}
             <Col className="text-center p-0 d-none d-lg-block d-xxl-none">
-                <Image src={img} alt="profile image" fluid className={styles.img} />
+                <Image src={ img } alt="profile image" fluid className={ styles.img } />
             </Col>
         </Row>
     )
