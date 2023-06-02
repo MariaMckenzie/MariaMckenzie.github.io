b89b3ece77115cff47eba736cc02162f1552798a
Maria McKenzie <mckenzie.maria.a@gmail.com>
2023-06-02 00:13:20 -0500

    update to add project details

--- src/components/project-details/qa.components.js
+++ src/components/project-details/qa.components.js
@@ -1,30 +1,13 @@
 import React from "react";
-import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import data from "../../assets/data/dev-projects.data.json";
-import img from "../../assets/images/temp.png";
-import styles from "./projectList.module.css";
+import styles from "./projectDetails.module.css";
 
-function QAProjectDetailsComponent() {
-    const fakeText = "Lorem ipsum dolor sit amet. Vel ipsa quis ea voluptas aliquam est explicabo maiores vel voluptate explicabo est velit odio At qui neque illum. Qui doloremque dolor ad tenetur eveniet sed facere galisum?";
-    const fakeTitle = "Lorem ipsum dolor sit amet";
+function QAProjectDetailsComponent () {
     return (
         <Container className="m-o p-3">
-            <h5 className={ styles.heading }>Frontend Development</h5>
-            <Row className="row-cols-auto text-start">
-                {/* add cards from json file */
-                    Array.from({ length: 3 }).map((_, id) => (
-                        <Col className="mx-auto">
-                            <Card className={ styles.card }>
-                                <Container className={ styles.cardDecor }></Container>
-                                <Card.Img src={img} alt="LinkedIn Icon" fluid className={ styles.img }/>
-                                <Card.Title className={ styles.title }> { fakeTitle } </Card.Title>
-                                <Card.Text className={ styles.text }> { fakeText } </Card.Text>
-                                <Button href={`/projects/development/${id+1}`} className={ styles.btn }>View Details</Button>
-                            </Card>
-                        </Col>
-                    ))
-                }
-            </Row>
+            <h3>Dev</h3>
         </Container>
     )
 }
---
+++ src/components/project-details/ui.component.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import data from "../../assets/data/dev-projects.data.json";
+import styles from "./projectDetails.module.css";
+
+function UIProjectDetailsComponent () {
+    return (
+        <Container className="m-o p-3">
+            <h3>Dev</h3>
+        </Container>
+    )
+}
+
+export default UIProjectDetailsComponent;
