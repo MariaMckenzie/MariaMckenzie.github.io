3bb2da97dc93b33d12148d59ae2933de63f3ece9
Maria McKenzie <mckenzie.maria.a@gmail.com>
2023-06-02 00:10:35 -0500

    update 404 page

--- src/pages/error/Error.page.js
+++ src/pages/error/Error.page.js
@@ -1,19 +1,27 @@
 import React from "react";
-import { Button, Container, Image } from "react-bootstrap";
+import { Button, Container, Image, Row } from "react-bootstrap";
+import FooterComponent from "../../components/footer/footer.component";
 import image from "../../assets/images/404.svg"
 import styles from "./Error.module.css";
 
 const errorPage = () => {
     return (
-        <>
-            <Container className={styles.container}>
+        <Container className="py-5">
+            <Container className={ styles.container }>
                 <Container className="text-center">
-                    <Image src={image} className={styles.image} alt="logo" />
-                    <h4 className={styles.text}>Page Not Found!</h4>
-                    <Button href="/" className={styles.btn}>Go to Home Page</Button>
+                    <Image src={ image } className={ styles.image } alt="logo" />
+                    <h4 className={ styles.text }>Page Not Found!</h4>
+                    <Button href="/" className={ styles.btn }>Go to Home Page</Button>
                 </Container>
             </Container>
-        </>
+            
+            {/* footer */}
+            <Container className={ styles.container }>
+                <Row className="mt-4">
+					<FooterComponent />
+				</Row>
+            </Container>
+        </Container>
     )
 }
 
