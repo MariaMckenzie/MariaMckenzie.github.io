11f2a4d732fb619f7852c03d8a9e0dad189e70e2
Maria McKenzie <mckenzie.maria.a@gmail.com>
2023-06-02 08:26:40 -0500

    fix responsiveness

--- src/components/footer/footer.component.js
+++ src/components/footer/footer.component.js
@@ -5,87 +5,75 @@ import githubIcon from "../../assets/icons/github.svg";
 import linkedinIcon from "../../assets/icons/linkedin.svg";
 import emailIcon from "../../assets/icons/email.svg";
 import fulllogo from "../../assets/images/full_logo.png";
-import data from "../../assets/data/about.data.json";
 import styles from "./footer.module.css";
 
 function FooterComponent () {
     return (
-            <Container>
-            {/* show for larger screens */}
-            <Container className="m-0 p-5 d-lg-none d-xxl-block">
-                <Row>
-                    <Col className="align-self-center">
-                        <Image src={fulllogo} alt="Logo" fluid className={ styles.logo } />
-                    </Col>
-                    <Col className="text-center align-self-center">
-                        <p className={ styles.text }> Location</p>
-                        <Container className="pb-4 d-block justify-content-center">
-                            <p className={ styles.text2 }>Kingston, Jamaica</p>
-                        </Container>
-                        <p className={ styles.text }> Contact Me</p>
-                        <Container className="d-flex justify-content-center">
-                            <a href={ data.github } target="_blank" rel="noopener noreferrer">
-                                <Image src={ githubIcon } alt="Github Icon" fluid className={ styles.icons } />
-                            </a>
-                            <a href={ data.linkedin } target="_blank" rel="noopener noreferrer">
-                                <Image src={ linkedinIcon } alt="LinkedIn Icon" fluid className={ styles.icons } />
-                            </a>
-                            <a href={ data.email } target="_blank" rel="noopener noreferrer">
-                                <Image src={ emailIcon } alt="Email Icon" fluid className={ styles.icons } />
-                            </a>       
-                        </Container>
-                    </Col>
-                    <Col className="align-self-center">
-                        <Container className="d-flex flex-column text-end ">
-                            <p className={ styles.text }>Important Links</p>
-                            <Link to="/" className={ styles.link }>Home</Link>
-                            <Link to="/portfolio" className={ styles.link }>Portfolio</Link>
-                            <Link to="/resume" className={ styles.link }>Resume</Link>
-                            {/* <Link to="/certificate" className={ styles.link }>Certificates</Link> */}
-                        </Container>
-                    </Col>
-                </Row>
-            </Container>
-            
-            {/* show for smaller screens */}
-            <Row className="m-0 p-5 d-none d-lg-block d-xxl-none text-center justify-content-center">
-                    <Container>
-                        <Image src={fulllogo} alt="Logo" fluid className={ styles.logo } />
-                    </Container>
-
-                    <Container className={ styles.footerDecor }></Container>
-
-                    <Container className="d-block">
-                        <p className={ styles.text }> Location</p>
+        <Container>
+        {/* show for larger screens */}
+        <Container className="m-0 p-5 d-none d-lg-block d-xxl-none">
+            <Row>
+                <Col className="align-self-center">
+                    <Image src={fulllogo} alt="Logo" fluid className={ styles.logo } />
+                </Col>
+                <Col className="text-center align-self-center">
+                    <p className={ styles.text }> Location</p>
+                    <Container className="pb-4 d-block justify-content-center">
                         <p className={ styles.text2 }>Kingston, Jamaica</p>
                     </Container>
-
-                    <Container className={ styles.footerDecor }></Container>
-
                     <p className={ styles.text }> Contact Me</p>
                     <Container className="d-flex justify-content-center">
-                        <a href={ data.github } target="_blank" rel="noopener noreferrer">
-                            <Image src={ githubIcon } alt="Github Icon" fluid className={ styles.icons } />
-                        </a>
-                        <a href={ data.linkedin } target="_blank" rel="noopener noreferrer">
-                            <Image src={ linkedinIcon } alt="LinkedIn Icon" fluid className={ styles.icons } />
-                        </a>
-                        <a href={ data.email } target="_blank" rel="noopener noreferrer">
-                            <Image src={ emailIcon } alt="Email Icon" fluid className={ styles.icons } />
-                        </a>                        
+                        <Image src={githubIcon} alt="Github Icon" fluid className={ styles.icons } />
+                        <Image src={linkedinIcon} alt="LinkedIn Icon" fluid className={ styles.icons } />
+                        <Image src={emailIcon} alt="Email Icon" fluid className={ styles.icons } />
                     </Container>
-
-                    <Container className={ styles.footerDecor }></Container>
-                    
-                    <Container className="d-flex flex-column pb-4">
+                </Col>
+                <Col className="align-self-center">
+                    <Container className="d-flex flex-column text-end ">
                         <p className={ styles.text }>Important Links</p>
                         <Link to="/" className={ styles.link }>Home</Link>
                         <Link to="/portfolio" className={ styles.link }>Portfolio</Link>
                         <Link to="/resume" className={ styles.link }>Resume</Link>
                         {/* <Link to="/certificate" className={ styles.link }>Certificates</Link> */}
                     </Container>
+                </Col>
             </Row>
         </Container>
+        
+
+        {/* show for smaller screens */}
+        <Row className="m-0 p-1 d-lg-none d-xxl-block text-center justify-content-center">
+                <Container>
+                    <Image src={fulllogo} alt="Logo" fluid className={ styles.logo } />
+                </Container>
+
+                <Container className={ styles.footerDecor }></Container>
+
+                <Container className="d-block">
+                    <p className={ styles.text }> Location</p>
+                    <p className={ styles.text2 }>Kingston, Jamaica</p>
+                </Container>
+
+                <Container className={ styles.footerDecor }></Container>
+
+                <p className={ styles.text }> Contact Me</p>
+                <Container className="d-flex justify-content-center">
+                    <Image src={githubIcon} alt="Github Icon" fluid className={ styles.icons } />
+                    <Image src={linkedinIcon} alt="LinkedIn Icon" fluid className={ styles.icons } />
+                    <Image src={emailIcon} alt="Email Icon" fluid className={ styles.icons } />
+                </Container>
+
+                <Container className={ styles.footerDecor }></Container>
+                
+                <Container className="d-flex flex-column pb-4">
+                    <p className={ styles.text }>Important Links</p>
+                    <Link to="/" className={ styles.link }>Home</Link>
+                    <Link to="/portfolio" className={ styles.link }>Portfolio</Link>
+                    <Link to="/resume" className={ styles.link }>Resume</Link>
+                    {/* <Link to="/certificate" className={ styles.link }>Certificates</Link> */}
+                </Container>
+        </Row>
+    </Container>
         
     )
 }
--- src/components/intro/intro.component.js
+++ src/components/intro/intro.component.js
@@ -15,11 +15,11 @@ function IntroComponent() {
 
             {/* show for smaller screens */}
             <Col className="d-lg-none d-xxl-block pb-4">
-                <Image src={ img } alt="profile image" fluid className={ styles.img } />
+                <Image src={img} alt="profile image" fluid className={styles.img} />
             </Col>
 
             <Col lg={7} className="p-0">
-                <h4 className={ styles.heading }>Hello I'm { intro_data.name.split(" ")[0] }</h4>
+                <h4 className={ styles.heading }>Hello I'm { data.name.split(" ")[0] }</h4>
                 <h2 className={ styles.subheading }>{ intro_data.about }</h2>
 
                 {/* show for smaller screens */}
@@ -36,7 +36,7 @@ function IntroComponent() {
                 </Container>
 
                 {/* buttons */}
-                <Container className="pt-1 d-block px-0 justify-content-center d-lg-none d-xxl-block pb-4">
+                <Container className="pt-1 d-flex flex-column px-0 justify-content-center d-lg-none d-xxl-block pb-4">
                     <Link to="/portfolio" className={ styles.btn1 }>View Portfolio</Link>
                     <Link to="/resume" className={ styles.btn2 }>View Resume</Link>
                 </Container>
@@ -51,7 +51,7 @@ function IntroComponent() {
                     </a>
                     <a href={ data.email } target="_blank" rel="noopener noreferrer">
                         <Image src={ emailIcon } alt="Email Icon" fluid className={ styles.icons } />
-                    </a>      
+                    </a>
                 </Container>
 
                 {/* buttons */}
@@ -59,14 +59,14 @@ function IntroComponent() {
                     <Link to="/portfolio" className={ styles.btn1 }>View Portfolio</Link>
                     <Link to="/resume" className={ styles.btn2 }>View Resume</Link>
                 </Container>
-
             </Col>
 
             {/* show for larger screens */}
             <Col className="text-center p-0 d-none d-lg-block d-xxl-none">
-                <Image src={ img } alt="profile image" fluid className={ styles.img } />
+                <Image src={img} alt="profile image" fluid className={styles.img} />
             </Col>
         </Row>
+
     )
 }
 
