b597b64b1d620534955fb02db3ebe10f5a1764c1
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2024-07-21 19:53:05 -0500

    remove unused code

--- src/components/project-list/ui.component.js
+++ src/components/project-list/ui.component.js
@@ -2,12 +2,9 @@ import React from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import data from "../../assets/data/ui-projects.data.json";
-import img from "../../assets/images/temp.png";
 import styles from "./projectList.module.css";
 
 function UIProjectListComponent() {
-    const fakeText = "Lorem ipsum dolor sit amet. Vel ipsa quis ea voluptas aliquam est explicabo maiores vel voluptate explicabo est velit odio At qui neque illum. Qui doloremque dolor ad tenetur eveniet sed facere galisum?";
-    const fakeTitle = "Lorem ipsum dolor sit amet";
     return (
         <Container className="m-o p-3">
             <h5 className={ styles.heading }>UI/UX Designs</h5>
