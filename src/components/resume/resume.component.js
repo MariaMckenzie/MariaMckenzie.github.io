b13151c107c8a86303bade68c30303ee7fd1946b
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2026-09-01 20:01:05 -0500

    update format

--- src/components/resume/resume.component.js
+++ src/components/resume/resume.component.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Card, Container, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Container, Row } from "react-bootstrap";
 import CertificatesSection from "./sections/certificates.section";
 import ExperienceSection from "./sections/experience.section";
 import EducationSection from "./sections/education.section";
@@ -9,7 +8,6 @@ import IntroSection from "./sections/intro.section";
 import SkillsSection from "./sections/skills.section";
 import SoftwareSection from "./sections/software.section";
 import SummarySection from "./sections/summary.section";
-import data from "../../assets/data/resume.data.json";
 import styles from "./resume.module.css";
 
 
@@ -33,54 +31,31 @@ function ResumeComponent() {
                 <ExperienceSection />
             </Row>
 
-            {/* education 
+            {/* education */}
             <Row className="pb-1">
                 <EducationSection />
             </Row>
 
-            {/* certificates 
+            {/* certificates */}
             <Row className="pb-1">
                 <CertificatesSection />
             </Row>
 
-            {/* skills 
+            {/* skills */}
             <Row className="pb-1">
                 <SkillsSection />
             </Row>
 
-            {/* software 
+            {/* software  */}
             <Row className="pb-1">
                 <SoftwareSection />
             </Row>
 
-            {/* interests 
+            {/* interests */}
             <Row className="pb-0">
                 <InterestsSection />
-            </Row> */}
+            </Row>
 
-
-
-             {/* <Row className="text-start"> */}
-                {/* add cards from json file */
-                    // Array.from({ length: data.section.length }).map((_, id) => (
-                    //     <Row key={`resume-${id}`} className="me-auto">
-                    //         <Card className={styles.card}>
-                    //             <Card.Title className={styles.title}> {data.section[id].name} </Card.Title>
-                    //             <Container className={styles.cardDecor}></Container>
-                    //             <Card.Text className={styles.text}><ul className={styles.text}>
-                    //                 {/* {data.section[id].info.map((bullet, idx) => (
-                    //                     <li key={idx}>{bullet}</li>
-                    //                 ))} */}
-                    //             </ul> </Card.Text>
-                    //         </Card>
-                    //     </Row>
-                    // ))
-                }
-            {/* </Row>  */}
-
-            {/* <Row>
-                <Link role="button" to="/portfolio" className={styles.btn}>View Projects</Link>
-            </Row> */}
         </Container>
     )
 }
