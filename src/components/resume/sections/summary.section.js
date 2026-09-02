f38e59650311463deaefd7a514eb4c37f274036a
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2026-09-01 19:27:17 -0500

    update formati for prof summary

--- src/components/resume/sections/summary.section.js
+++ src/components/resume/sections/summary.section.js
@@ -5,79 +5,10 @@ import styles from "../resume.module.css";
 
 function SummarySection() {
     return (
-        <Container className={[styles.container, "m-0"].join(" ")}>
-            {/* professional summary */}
-            <Row className="pb-4">
-                <h6 className={styles.text}>{data.section[1].name}</h6>
-                <Container className={styles.cardDecor}></Container>
-                <p className={styles.text}>{data.section[1].info}</p>
-            </Row>
-
-            {/* work experience */}
-            <Row className="pb-4">
-                <h6 className={styles.text}>{data.section[2].name}</h6>
-                <Container className={styles.cardDecor}></Container>
-                <p className={styles.text}>{data.section[2].info}</p>
-            </Row>
-
-            {/* education */}
-            <Row className="pb-4">
-                <h6 className={styles.text}>{data.section[1].name}</h6>
-                <Container className={styles.cardDecor}></Container>
-                <p className={styles.text}>{data.section[1].info}</p>
-            </Row>
-
-            {/* certificates */}
-            <Row className="pb-4">
-                <h6 className={styles.text}>{data.section[1].name}</h6>
-                <Container className={styles.cardDecor}></Container>
-                <p className={styles.text}>{data.section[1].info}</p>
-            </Row>
-
-            {/* skills */}
-            <Row className="pb-4">
-                <h6 className={styles.text}>{data.section[1].name}</h6>
-                <Container className={styles.cardDecor}></Container>
-                <p className={styles.text}>{data.section[1].info}</p>
-            </Row>
-
-            {/* software */}
-            <Row className="pb-4">
-                <h6 className={styles.text}>{data.section[1].name}</h6>
-                <Container className={styles.cardDecor}></Container>
-                <p className={styles.text}>{data.section[1].info}</p>
-            </Row>
-
-            {/* interests */}
-            <Row className="pb-4">
-                <h6 className={styles.text}>{data.section[1].name}</h6>
-                <Container className={styles.cardDecor}></Container>
-                <p className={styles.text}>{data.section[1].info}</p>
-            </Row>
-
-
-
-            <Row className="text-start">
-                {/* add cards from json file */
-                    Array.from({ length: data.section.length }).map((_, id) => (
-                        <Row key={`resume-${id}`} className="me-auto">
-                            <Card className={styles.card}>
-                                <Card.Title className={styles.title}> {data.section[id].name} </Card.Title>
-                                <Container className={styles.cardDecor}></Container>
-                                <Card.Text className={styles.text}><ul className={styles.text}>
-                                    {data.section[id].info.map((bullet, idx) => (
-                                        <li key={idx}>{bullet}</li>
-                                    ))}
-                                </ul> </Card.Text>
-                            </Card>
-                        </Row>
-                    ))
-                }
-            </Row>
-
-            <Row>
-                <Link role="button" to="/portfolio" className={styles.btn}>View Projects</Link>
-            </Row>
+        <Container className="m-0">
+            <h6 className={styles.heading}>{data.summary.title}</h6>
+            <p className={styles.text}>{data.summary.text}
+            </p>
         </Container>
     )
 }
