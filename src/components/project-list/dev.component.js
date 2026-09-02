86b95838a5f3b30779f6f4b168d5f2f87ea9da7d
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2026-09-02 18:43:04 -0500

    update

--- src/components/project-list/dev.component.js
+++ src/components/project-list/dev.component.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import data from "../../assets/data/dev-projects.data.json";
 import styles from "./projectList.module.css";
 
