678f29d3cd8b8a5518d68c13a880b93365fad2c3
MariaMckenzie <68311077+MariaMckenzie@users.noreply.github.com>
2026-09-01 19:31:54 -0500

    update exp component

--- src/components/resume/sections/experience.section.js
+++ src/components/resume/sections/experience.section.js
@@ -1,82 +1,37 @@
-import { Card, Container, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import React from "react";
+import { Container, Row } from "react-bootstrap";
 import data from "../../../assets/data/resume.data.json";
 import styles from "../resume.module.css";
 
 function ExperienceSection() {
     return (
-        <Container className={[styles.container, "m-0"].join(" ")}>
-            {/* professional summary */}
-            <Row className="pb-4">
-                <h6 className={styles.text}>{data.section[1].name}</h6>
-                <Container className={styles.cardDecor}></Container>
-                <p className={styles.text}>{data.section[1].info}</p>
-            </Row>
-
-            {/* work experience */}
-            <Row className="pb-4">
-                <h6 className={styles.text}>{data.section[2].name}</h6>
-                <Container className={styles.cardDecor}></Container>
-                <p className={styles.text}>{data.section[2].info}</p>
-            </Row>
-
-            {/* education */}
-            <Row className="pb-4">
-                <h6 className={styles.text}>{data.section[1].name}</h6>
-                <Container className={styles.cardDecor}></Container>
-                <p className={styles.text}>{data.section[1].info}</p>
-            </Row>
-
-            {/* certificates */}
-            <Row className="pb-4">
-                <h6 className={styles.text}>{data.section[1].name}</h6>
-                <Container className={styles.cardDecor}></Container>
-                <p className={styles.text}>{data.section[1].info}</p>
-            </Row>
-
-            {/* skills */}
-            <Row className="pb-4">
-                <h6 className={styles.text}>{data.section[1].name}</h6>
-                <Container className={styles.cardDecor}></Container>
-                <p className={styles.text}>{data.section[1].info}</p>
-            </Row>
-
-            {/* software */}
-            <Row className="pb-4">
-                <h6 className={styles.text}>{data.section[1].name}</h6>
-                <Container className={styles.cardDecor}></Container>
-                <p className={styles.text}>{data.section[1].info}</p>
-            </Row>
-
-            {/* interests */}
-            <Row className="pb-4">
-                <h6 className={styles.text}>{data.section[1].name}</h6>
-                <Container className={styles.cardDecor}></Container>
-                <p className={styles.text}>{data.section[1].info}</p>
-            </Row>
-
-
+        <Container className="m-0">
+            <h6 className={styles.text}>{data.experience.title}</h6>
+            <Container className={styles.cardDecor}></Container>
 
             <Row className="text-start">
-                {/* add cards from json file */
-                    Array.from({ length: data.section.length }).map((_, id) => (
-                        <Row key={`resume-${id}`} className="me-auto">
-                            <Card className={styles.card}>
-                                <Card.Title className={styles.title}> {data.section[id].name} </Card.Title>
-                                <Container className={styles.cardDecor}></Container>
-                                <Card.Text className={styles.text}><ul className={styles.text}>
-                                    {data.section[id].info.map((bullet, idx) => (
+                {/* add information from json file */
+                    Array.from({ length: data.experience.info.length }).map((_, id) => (
+                        <Row key={`exp-${id}`} className="me-auto">
+                            <p className={styles.text}>
+                                <span>{data.experience.info[id].role}</span>
+                                <span className="px-2">|</span>
+                                <span>{data.experience.info[id].startDate} - {data.experience.info[id].endDate}</span>
+                                <span className="px-2">|</span>
+                                <span>{data.experience.info[id].company} ({data.experience.info[id].context})</span>
+                                <span className="px-2">|</span>
+                                <span>{data.experience.info[id].location}</span>
+                            </p>
+                            <Container></Container>
+                            <p className={styles.text}>
+                                <ul className={styles.text}>
+                                    {data.experience.info[id].bullets.map((bullet, idx) => (
                                         <li key={idx}>{bullet}</li>
                                     ))}
-                                </ul> </Card.Text>
-                            </Card>
+                                </ul> </p>
                         </Row>
                     ))
                 }
-            </Row>
-
-            <Row>
-                <Link role="button" to="/portfolio" className={styles.btn}>View Projects</Link>
             </Row>
         </Container>
     )
